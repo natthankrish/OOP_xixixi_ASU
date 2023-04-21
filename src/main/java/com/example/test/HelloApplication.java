8a9600844a18fa8708df768fed49b612c5183b57
Gulilil <juan.csantoso@gmail.com>
2023-04-22 01:57:25 +0700

    further delete fxml file

--- src/main/java/com/example/test/HelloApplication.java
+++ src/main/java/com/example/test/HelloApplication.java
@@ -1,7 +1,6 @@
 package com.example.test;
 
 import javafx.application.Application;
-// import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,9 +9,6 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        // FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        // Scene scene = new Scene(fxmlLoader.load(), 1200, 720);
-        // stage.setScene(scene);
         stage.setTitle("Hello!");
         stage.show();
         TestLombok lombok = new TestLombok(1, 1);
