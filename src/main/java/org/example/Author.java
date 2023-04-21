912d313fde162cfe98b5e5b5930a7f2d2d085b25
BrigitaCarolina <16521249@mahasiswa.itb.ac.id>
2023-04-21 23:25:28 +0700

    init project

---
+++ src/main/java/org/example/Author.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Author {
+    private int id;
+    private String name;
+    private String surname;
+}
+
