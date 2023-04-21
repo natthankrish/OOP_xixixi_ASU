89893e3c1642857e11001c6330bf462c9c1288e1
BrigitaCarolina <16521249@mahasiswa.itb.ac.id>
2023-04-22 02:09:45 +0700

    update: module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,9 @@
 module com.example.test {
     requires javafx.controls;
-    requires javafx.fxml;
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
     requires lombok;
 
-    opens com.example.test to javafx.fxml;
     exports com.example.test;
 }
