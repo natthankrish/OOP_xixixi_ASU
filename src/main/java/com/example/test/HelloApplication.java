1f80a68b073e0eb5c0006d207aa1b20923f1e021
Gulilil <juan.csantoso@gmail.com>
2023-04-22 01:46:55 +0700

    fix: disabling fxml file

--- src/main/java/com/example/test/HelloApplication.java
+++ src/main/java/com/example/test/HelloApplication.java
@@ -1,7 +1,7 @@
 package com.example.test;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+// import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,10 +10,10 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1200, 720);
+        // FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        // Scene scene = new Scene(fxmlLoader.load(), 1200, 720);
+        // stage.setScene(scene);
         stage.setTitle("Hello!");
-        stage.setScene(scene);
         stage.show();
         TestLombok lombok = new TestLombok(1, 1);
         System.out.println(lombok.getId() + " " + lombok.getStock());
