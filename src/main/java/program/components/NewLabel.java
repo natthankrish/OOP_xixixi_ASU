5565eb978acd4f7d286961abb40b318b5069dccf
BrigitaCarolina <16521249@mahasiswa.itb.ac.id>
2023-04-23 23:07:40 +0700

    change: label

--- src/main/java/program/components/NewLabel.java
+++ src/main/java/program/components/NewLabel.java
@@ -1,54 +1,47 @@
 package program.components;
-import javafx.scene.Node;
 import javafx.scene.control.Label;
-import org.jetbrains.annotations.NotNull;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import lombok.Getter;
+import lombok.Setter;
 
-public class NewLabel extends Label {
-    public NewLabel(String text, @NotNull String header, String color) {
-        super(text);
-        String style = "";
-        if (header.equals("h1")) {
-            if (color.equals("brown")) {
-                style = "-fx-font-family: Inter; -fx-font-style: normal; -fx-font-weight: 700; -fx-font-size: 48px; -fx-text-fill: #867070;";
-            } else if (color.equals("white")) {
-                style = "-fx-font-family: Inter; -fx-font-style: normal; -fx-font-weight: 700; -fx-font-size: 48px; -fx-text-fill: #FFFFFF;";
-            }
-        } else if (header.equals("h2")) {
-            if (color.equals("brown")) {
-                style = "-fx-font-family: Inter; -fx-font-style: normal; -fx-font-weight: 700; -fx-font-size: 32px; -fx-text-fill: #867070;";
-            } else if (color.equals("white")) {
-                style = "-fx-font-family: Inter; -fx-font-style: normal; -fx-font-weight: 700; -fx-font-size: 32px; -fx-text-fill: #FFFFFF;";
-            }
-        } else if (header.equals("h3")) {
-            if (color.equals("brown")) {
-                style = "-fx-font-family: Inter; -fx-font-style: normal; -fx-font-weight: 700; -fx-font-size: 24px; -fx-text-fill: #867070;";
-            } else if (color.equals("white")) {
-                style = "-fx-font-family: Inter; -fx-font-style: normal; -fx-font-weight: 700; -fx-font-size: 24px; -fx-text-fill: #FFFFFF;";
-            }
-        } else if (header.equals("h4")) {
-            if (color.equals("brown")) {
-                style = "-fx-font-family: Inter; -fx-font-style: normal; -fx-font-weight: 700; -fx-font-size: 19px; -fx-text-fill: #867070;";
-            } else if (color.equals("putih")) {
-                style = "-fx-font-family: Inter; -fx-font-style: normal; -fx-font-weight: 700; -fx-font-size: 19px; -fx-text-fill: #FFFFFF;";
-            }
-        } else if (header.equals("text2")) {
-            if (color.equals("brown")) {
-                style = "-fx-font-family: Inter; -fx-font-style: normal; -fx-font-weight: 700; -fx-font-size: 12px; -fx-text-fill: #867070;";
-            } else if (color.equals("white")) {
-                style = "-fx-font-family: Inter; -fx-font-style: normal; -fx-font-weight: 700; -fx-font-size: 12px; -fx-text-fill: #FFFFFF;";
-            }
-        } else if (header.equals("text1")) {
-            if (color.equals("brown")) {
-                style = "-fx-font-family: Inter; -fx-font-style: normal; -fx-font-weight: 700; -fx-font-size: 15px; -fx-text-fill: #867070;";
-            } else if (color.equals("white")) {
-                style = "-fx-font-family: Inter; -fx-font-style: normal; -fx-font-weight: 700; -fx-font-size: 15px; -fx-text-fill: #FFFFFF;";
-            }
-        }
-        super.setStyle(style);
+@Setter
+@Getter
+public class NewLabel extends Label{
+    private String labelText;
+    private int size; // in pixel
+    private String color;
+    private String bgColor;
+    private int weight;
+
+    public NewLabel() {
+        super("");
+        this.size = 0;
+        this.weight = 0;
     }
 
-    @Override
-    public Node getStyleableNode() {
-        return super.getStyleableNode();
+    public NewLabel(String text, int size, String color, String bgColor, int weight) {
+        super("");
+        this.labelText = text;
+        this.size = size;
+        this.color = color;
+        this.bgColor = bgColor;
+        this.weight = weight;
+    }
+    public void setStyle() {
+        if (this.labelText != null) {
+            setText(this.labelText);
+        }
+        if (this.color != null) {
+            Color color = Color.web(this.color);
+            setTextFill(color);
+        }
+        if (this.size != 0) {
+            int fontSizeInPoint = (int)(this.size * 0.75);
+            setFont(new Font("Inter", fontSizeInPoint));
+        }
+        if (this.weight != 0) {
+            setStyle(String.format("-fx-font-weight: %d;", this.weight));
+        }
     }
 }
