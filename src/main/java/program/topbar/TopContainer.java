a234bd5f51abb24ddce5a789eaa78c2400ea6a1a
melvinkj <melvinkentj@gmail.com>
2023-04-25 01:36:12 +0700

    fix: fix previously wrong merging

--- src/main/java/program/topbar/TopContainer.java
+++ src/main/java/program/topbar/TopContainer.java
@@ -2,22 +2,26 @@ package program.topbar;
 
 import javafx.scene.Group;
 import javafx.stage.Screen;
-
 import lombok.*;
 import program.components.Background;
 import program.components.NewImage;
 
 @Getter
 public class TopContainer extends Group {
+    private TopbarFunctional topbar;
     private Background component;
     private NewImage logo;
     private LogoThread logoThread;
-    private  TopbarFunctional topbar;
     public TopContainer() {
-        this.component = new Rectangle();
-        this.component.setWidth(Screen.getPrimary().getVisualBounds().getWidth());
-        this.component.setHeight(Screen.getPrimary().getVisualBounds().getHeight() / 15);
-        this.component.setFill(Color.valueOf("#E4D0D0"));
-        this.getChildren().add(this.component);
+        this.topbar = new TopbarFunctional();
+        this.component = new Background(Screen.getPrimary().getVisualBounds().getWidth(), Screen.getPrimary().getVisualBounds().getHeight() / 15, "#E4D0D0");
+        this.getChildren().addAll(this.component, this.topbar);
+
+        this.logo = new NewImage("assets/logo/1.png");
+        this.logo.setDimension(this.component.getHeight() * 32 / 27, this.component.getHeight() * 2 / 3);
+        this.logo.setPosition(this.component.getWidth() - (this.logo.getFitWidth() * 1.5), this.component.getHeight()/2 - this.logo.getFitHeight()/2);
+        this.getChildren().add(this.logo);
+
+        this.logoThread = new LogoThread(this.logo);
     }
 }
