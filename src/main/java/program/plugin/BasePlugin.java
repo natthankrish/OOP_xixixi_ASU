62f7c800936a2eb0a7e1ac6ca805a5562141b293
BrigitaCarolina <16521249@mahasiswa.itb.ac.id>
2023-05-03 04:02:15 +0700

    baseplugin

--- src/main/java/program/plugin/BasePlugin.java
+++ src/main/java/program/plugin/BasePlugin.java
@@ -1,6 +1,18 @@
 package program.plugin;
 
 
-public class BasePlugin{
+
+public class BasePlugin {
+    // show empty page
+
+    public void addLineChart() {
+        // add line chart to the page
+    }
+    public void addBarChart() {
+        // add bar chart to the page
+    }
+    public void addPieChart() {
+        // add the pie chart to the page
+    }
 
 }
