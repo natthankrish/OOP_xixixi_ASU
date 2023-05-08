d49a2ee5a1604696c061b33884311bd2daa6eff5
egijago <16521433@mahasiswa.itb.ac.id>
2023-05-08 08:01:09 +0700

    adjust size

--- Main/src/main/java/org/example/program/components/CloseAllButton.java
+++ Main/src/main/java/org/example/program/components/CloseAllButton.java
@@ -1,18 +1,33 @@
 package org.example.program.components;
+import javafx.animation.ScaleTransition;
 import javafx.scene.control.Button;
+import javafx.util.Duration;
 
 public class CloseAllButton extends Button {
     public CloseAllButton() {
         setStyle("""
             -fx-text-fill: #F5EBEB;
             -fx-background-color: #867070;
-            -fx-pref-height: 23.3;
-            -fx-pref-width: 72;
-            -fx-font-size: 8;
+            -fx-pref-height: 35;
+            -fx-pref-width: 80;
+            -fx-font-size: 12;
             -fx-font-weight: bold;
-            -fx-background-radius: 9;   
+            -fx-background-radius: 14;   
         """);
         this.setText("Close All");
+        ScaleTransition scaleTransition = new ScaleTransition(new Duration(0.2), this);
+        scaleTransition.setFromX(1);
+        scaleTransition.setFromY(1);
+        scaleTransition.setToX(0.8);
+        scaleTransition.setToY(0.8);
+        scaleTransition.setAutoReverse(true);
+        this.setOnMousePressed(event -> scaleTransition.playFromStart());
+        this.setOnMouseReleased(event -> {
+            scaleTransition.stop();
+            this.setScaleX(1);
+            this.setScaleY(1);
+        });
+
     }
 
     public void setLayout(double x, double y) {
